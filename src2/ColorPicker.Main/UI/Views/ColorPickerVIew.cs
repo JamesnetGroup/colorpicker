485a3af3f3f0fad938dbd4c1c58fa51c0ecba465
james lee <james.lee@devncore.org>
2021-10-22 06:53:52 +0900

    Add ColorPickerView

--- src2/ColorPicker.Main/UI/Views/ColorPickerVIew.cs
+++ src2/ColorPicker.Main/UI/Views/ColorPickerVIew.cs
@@ -1,8 +1,25 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
-namespace ColorPicker.Main.UI.Views 
+namespace ColorPicker.Main.UI.Views
 {
-    public class ColorPickerVIew : Window
+    public class ColorPickerView : Window
     {
+        static ColorPickerView()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ColorPickerView), new FrameworkPropertyMetadata(typeof(ColorPickerView)));
+        }
     }
 }
