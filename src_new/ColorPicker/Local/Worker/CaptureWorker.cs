3aa30c6bcf364c940544befa3569d144aeddd463
devncore-elena <elena.kim@devncore.org>
2022-01-06 23:46:19 +0900

    Change to new version

---
+++ src_new/ColorPicker/Local/Worker/CaptureWorker.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace ColorPicker.Local.Worker
+{
+    internal class CaptureWorker
+    {
+
+        [DllImport("user32.dll")]
+        static extern bool GetCursorInfo(out CURSORINFO pci);
+
+        [DllImport("user32.dll")]
+        static extern bool DrawIcon(IntPtr hDC, int X, int Y, IntPtr hIcon);
+
+        const Int32 CURSOR_SHOWING = 0x00000001;
+
+        public static Bitmap CaptureScreen(bool CaptureMouse)
+        {
+            Bitmap result = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height, PixelFormat.Format24bppRgb);
+
+            try
+            {
+                using (Graphics g = Graphics.FromImage(result))
+                {
+                    g.CopyFromScreen(0, 0, 0, 0, Screen.PrimaryScreen.Bounds.Size, CopyPixelOperation.SourceCopy);
+
+                    if (CaptureMouse)
+                    {
+                        CURSORINFO pci;
+                        pci.cbSize = System.Runtime.InteropServices.Marshal.SizeOf(typeof(CURSORINFO));
+
+                        if (GetCursorInfo(out pci))
+                        {
+                            if (pci.flags == CURSOR_SHOWING)
+                            {
+                                DrawIcon(g.GetHdc(), pci.ptScreenPos.x, pci.ptScreenPos.y, pci.hCursor);
+                                g.ReleaseHdc();
+                            }
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                result = null;
+            }
+
+            return result;
+        }
+    }
+    [StructLayout(LayoutKind.Sequential)]
+    struct CURSORINFO
+    {
+        public Int32 cbSize;
+        public Int32 flags;
+        public IntPtr hCursor;
+        public POINTAPI ptScreenPos;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    struct POINTAPI
+    {
+        public int x;
+        public int y;
+    }
+}
